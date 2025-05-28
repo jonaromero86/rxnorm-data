3053b769d9b4613dbb402ba13dcb83bc407d4b52
Jonathan Romero <141253593+jonaromero86@users.noreply.github.com>
2025-05-28 15:14:02 -0400

    Removing unneeded references

--- rxnorm-integration/src/test/java/dev/ikm/tinkar/rxnorm/integration/RxnormDescriptionSemanticIT.java
+++ rxnorm-integration/src/test/java/dev/ikm/tinkar/rxnorm/integration/RxnormDescriptionSemanticIT.java
@@ -66,6 +66,10 @@ public class RxnormDescriptionSemanticIT extends AbstractIntegrationTest {
             AtomicBoolean rxnormSynonym = new AtomicBoolean(!rxnormData.getRxnormSynonym().isEmpty());
             AtomicBoolean rxnormPrescribableSynonym = new AtomicBoolean(!rxnormData.getPrescribableSynonym().isEmpty());
             AtomicReference<List<String>> rxnormTallmanSynonym = new AtomicReference<>();
+            AtomicBoolean matchedName = new AtomicBoolean(!rxnormName.get());
+            AtomicBoolean matchedSynonym = new AtomicBoolean(!rxnormSynonym.get());
+            AtomicBoolean matchedPrescribableSynonym = new AtomicBoolean(!rxnormPrescribableSynonym.get());
+            AtomicInteger matchedTallmanSynonyms = new AtomicInteger(0);
 
             if (!rxnormData.getTallmanSynonyms().isEmpty()) {
                 rxnormTallmanSynonym.set(rxnormData.getTallmanSynonyms());
@@ -73,28 +77,15 @@ public class RxnormDescriptionSemanticIT extends AbstractIntegrationTest {
                 rxnormTallmanSynonym.set(new ArrayList<>());
             }
 
-            AtomicBoolean matchedName = new AtomicBoolean(!rxnormName.get());
-            AtomicBoolean matchedSynonym = new AtomicBoolean(!rxnormSynonym.get());
-            AtomicBoolean matchedPrescribableSynonym = new AtomicBoolean(!rxnormPrescribableSynonym.get());
-            AtomicInteger matchedTallmanSynonyms = new AtomicInteger(0);
-
-            AtomicInteger counter = new AtomicInteger(0);
-
             EntityService.get().forEachSemanticForComponentOfPattern(concept.nid(), TinkarTerm.DESCRIPTION_PATTERN.nid(), semanticEntity -> {
                 Latest<SemanticEntityVersion> latestActive = stampCalcActive.latest(semanticEntity);
 
-                counter.incrementAndGet();
                 if (latestActive.isPresent()) {
                     String textForDesc = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.TEXT_FOR_DESCRIPTION, latestActive.get());
                     Component descCaseSignificance = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_CASE_SIGNIFICANCE, latestActive.get());
                     Component descType = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_TYPE, latestActive.get());
 
                     if (rxnormName.get()) {
-                        if (rxnormData.getId().equals("1000001")) {
-                            LOG.info("***JTD: rxnormName " + rxnormData.getRxnormName() + ", " + textForDesc + ": "+textForDesc.equals(rxnormData.getRxnormName()));
-                            LOG.info("***JTD: rxnormName desc case " + DESCRIPTION_NOT_CASE_SENSITIVE + ", " + descCaseSignificance + ": " + descCaseSignificance.equals(DESCRIPTION_NOT_CASE_SENSITIVE));
-                            LOG.info("***JTD: rxnormName desc type " + FULLY_QUALIFIED_NAME_DESCRIPTION_TYPE + ", " + descType + ": "+(descType.equals(FULLY_QUALIFIED_NAME_DESCRIPTION_TYPE)));
-                        }
                         if (textForDesc.equals(rxnormData.getRxnormName())
                                 && descCaseSignificance.equals(DESCRIPTION_NOT_CASE_SENSITIVE)
                                 && descType.equals(FULLY_QUALIFIED_NAME_DESCRIPTION_TYPE)) {
@@ -104,12 +95,6 @@ public class RxnormDescriptionSemanticIT extends AbstractIntegrationTest {
                     }
 
                     if (rxnormSynonym.get()) {
-                        if (rxnormData.getId().equals("1000001")) {
-                            LOG.info("***JTD: rxnormSynonym " + rxnormData.getRxnormSynonym() + ", " + textForDesc + ": "+textForDesc.equals(rxnormData.getRxnormSynonym()));
-                            LOG.info("***JTD: rxnormSynonym desc case " + DESCRIPTION_NOT_CASE_SENSITIVE + ", " + descCaseSignificance + ": " + descCaseSignificance.equals(DESCRIPTION_NOT_CASE_SENSITIVE));
-                            LOG.info("***JTD: rxnormSynonym desc type " + REGULAR_NAME_DESCRIPTION_TYPE + ", " + descType + ": "+(descType.equals(REGULAR_NAME_DESCRIPTION_TYPE)));
-                        }
-
                         if (textForDesc.equals(rxnormData.getRxnormSynonym())
                                 && descCaseSignificance.equals(DESCRIPTION_NOT_CASE_SENSITIVE)
                                 && (descType.equals(REGULAR_NAME_DESCRIPTION_TYPE))) {
@@ -119,11 +104,6 @@ public class RxnormDescriptionSemanticIT extends AbstractIntegrationTest {
                     }
 
                     if (rxnormPrescribableSynonym.get()) {
-                        if (rxnormData.getId().equals("1000001")) {
-                            LOG.info("***JTD: rxnormPrescribableSynonym " + rxnormData.getRxnormSynonym() + ", " + textForDesc + ": "+textForDesc.equals(rxnormData.getPrescribableSynonym()));
-                            LOG.info("***JTD: rxnormPrescribableSynonym desc case " + DESCRIPTION_NOT_CASE_SENSITIVE + ", " + descCaseSignificance + ": " + descCaseSignificance.equals(DESCRIPTION_NOT_CASE_SENSITIVE));
-                            LOG.info("***JTD: rxnormPrescribableSynonym desc type " + REGULAR_NAME_DESCRIPTION_TYPE + ", " + descType + ": "+(descType.equals(REGULAR_NAME_DESCRIPTION_TYPE)));
-                        }
                         if (textForDesc.equals(rxnormData.getPrescribableSynonym())
                                 && descCaseSignificance.equals(DESCRIPTION_NOT_CASE_SENSITIVE)
                                 && (descType.equals(REGULAR_NAME_DESCRIPTION_TYPE))) {
@@ -135,56 +115,18 @@ public class RxnormDescriptionSemanticIT extends AbstractIntegrationTest {
                     List<String> synonyms = rxnormTallmanSynonym.get();
                     if (!synonyms.isEmpty()) {
                         synonyms.forEach(synonym -> {
-                            if (rxnormData.getId().equals("1000001")) {
-                                LOG.info("***JTD: rxnormTallmanSynonym " + rxnormData.getRxnormSynonym() + ", " + textForDesc + ": "+textForDesc.equals(synonym));
-                                LOG.info("***JTD: rxnormTallmanSynonym desc case " + DESCRIPTION_CASE_SENSITIVE + ", " + descCaseSignificance + ": " + descCaseSignificance.equals(DESCRIPTION_CASE_SENSITIVE));
-                                LOG.info("***JTD: rxnormTallmanSynonym desc type " + REGULAR_NAME_DESCRIPTION_TYPE + ", " + descType + ": "+(descType.equals(REGULAR_NAME_DESCRIPTION_TYPE)));
-                            }
                             if (textForDesc.equals(synonym)
                                     && descCaseSignificance.equals(DESCRIPTION_CASE_SENSITIVE)
                                     && descType.equals(REGULAR_NAME_DESCRIPTION_TYPE)) {
                                 matchedTallmanSynonyms.incrementAndGet();
-                                if (rxnormData.getId().equals("1000001")) {
-                                    LOG.info("***JTD: matched " + matchedTallmanSynonyms.get());
-                                }
-                            } else  if (rxnormData.getId().equals("1000001")) {
-                                LOG.info("***JTD: no match " +textForDesc.equals(synonym) +", "+descCaseSignificance.equals(DESCRIPTION_CASE_SIGNIFICANCE)+", "+descType.equals(REGULAR_NAME_DESCRIPTION_TYPE));
                             }
                         });
                     }
                 }
-                if (rxnormData.getId().equals("1000001")) {
-                    LOG.info("***JTD: count is " + counter.get());
-                }
             });
 
-            if (RxnormDescriptionSemanticIT.count < 1) {
-                if (!(matchedName.get() && matchedSynonym.get() && matchedPrescribableSynonym.get() && matchedTallmanSynonyms.get() == rxnormTallmanSynonym.get().size())) {
-                    LOG.info("***JTD: matchedName.get(): " + matchedName.get() + " - matchedSynonym.get(): " + matchedSynonym.get() + " - matchedPrescribableSynonym.get(): " + matchedPrescribableSynonym.get() + " - matchedTallman " + matchedTallmanSynonyms.get() + " =? " + rxnormTallmanSynonym.get().size());
-                    LOG.info("***JTD: concept " + concept + ", concept publicId " + concept.publicId());
-                    LOG.info("***JTD: id " + rxnormData.getId() + " name " + rxnormData.getRxnormName() + ", synonym " + rxnormData.getRxnormSynonym() + ", prescribable "+rxnormData.getPrescribableSynonym());
-                    EntityProxy.Semantic semantic = EntityProxy.Semantic.make(
-                            PublicIds.of(UuidT5Generator.get(UUID.fromString(namespaceString), concept.publicId().asUuidArray()[0] + rxnormData.getRxnormName() + "DESC")));
-                    LOG.info("***JTD: semantic for name " + semantic);
-                    Entity<SemanticEntityVersion> e = EntityService.get().getEntityFast(UuidT5Generator.get(UUID.fromString(namespaceString), concept.publicId().asUuidArray()[0] + rxnormData.getRxnormName() + "DESC"));
-                    if (e == null) {
-                        LOG.error("***JTD: semantic entity not found");
-                    } else {
-                        LOG.info("***JTD: semantic entity found "+e);
-                        LOG.info("***JTD: latest active " + stampCalcActive.latest(e));
-                        LOG.info("***JTD: reference: " +stampCalcActive.latest(e).get().referencedComponent());
-                        LOG.info("***JTD: concept: " + stampCalcActive.latest(concept));
-                        LOG.info("***JTD: pattern: " + stampCalcActive.latest(e).get().pattern());
-                    }
-                    LOG.info("***JTD: trying to find  semantic with " + namespaceString + ", " + concept.publicId().asUuidArray()[0] + rxnormData.getRxnormName() + "DESC");
-                    LOG.info("***JTD: pattern " + TinkarTerm.DESCRIPTION_PATTERN);
-
-                    RxnormDescriptionSemanticIT.count++;
-                }
-            }
             return (matchedName.get() && matchedSynonym.get() && matchedPrescribableSynonym.get() && matchedTallmanSynonyms.get() == rxnormTallmanSynonym.get().size());
         }
         return false; // TOTAL 28
     }
-    static int count = 0;
 }
